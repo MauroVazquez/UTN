0ee2b791a504adcf0838bb7b4a61295460b393b7
MauroVazquez <36246947+MauroVazquez@users.noreply.github.com>
2018-02-16 21:08:51 -0300

    Add files via upload

---
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -0,0 +1,11 @@
+function Mostrar()
+{
+	var num = 1;
+
+	while (num <= 10) {
+		 console.log(num)
+		 num++
+	}
+
+
+}//FIN DE LA FUNCIÓN
---
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -0,0 +1,12 @@
+function Mostrar()
+	{
+
+		var num = 10;
+		
+			while (num >=1  ) 
+			{
+				 console.log(num)
+				 num--
+			}
+
+}//FIN DE LA FUNCIÓN
