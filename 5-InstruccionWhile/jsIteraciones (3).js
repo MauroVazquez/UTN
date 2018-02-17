9195731d7ef911cc6a6e4e5c109e66ed245dc088
MauroVazquez <36246947+MauroVazquez@users.noreply.github.com>
2018-02-16 22:11:42 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,6 +2,9 @@ function Mostrar()
 {
 
 var clave = prompt("ingrese el número clave.");
-
-
+while(!(clave=="utn750"))
+{
+    clave = prompt("Error Reingrese la clave")
+}
+alert("Clave correcta!!!")
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,10 @@
 function Mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
-
-
+	var numero = prompt("ingrese un número entre 1 y 10.");
+while(!(numero >=1 && numero <=10 ))
+{
+  numero = prompt("Nota invalida. Reingrese nota")
+}
+document.getElementById("numero") =
 }//FIN DE LA FUNCIÓN
