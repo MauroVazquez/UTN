cca52a1d9ac4478fd7484f73431c556cf642e298
MauroVazquez <36246947+MauroVazquez@users.noreply.github.com>
2018-02-23 22:11:31 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,70 @@
+/*1-Suma de los negativos.
+ 2-Suma de los positivos.
+  3-Cantidad de positivos. 
+  4-Cantidad de negativos. 
+  5-Cantidad de ceros. 
+  6-Cantidad de números pares.
+  7-Promedio de positivos.
+  8-Promedios de negativos.
+   9-Diferencia entre positivos y negativos, (positvos-negativos). */
+
 function Mostrar()
 {
-
-	var contador=0;
-	//declarar contadores y variables 
+	var acumuladorNeg = 0;
+	var acumuladorPos =0;
+	var contadorPositivos=0;
+	var contadorNegativos=0;
+	var contadorCeros=0;
+	var contadorPares=0;
+	var promedioPos =0;
+	var promedioNeg =0;
+	var diferencia;
+	var numero;
+	var seguir= "s";
 	
-	var respuesta="si";
 
-	while(respuesta!="no")
+	do
+	{	
+		numero =parseInt(prompt("ingrese un numero"));
+		if(numero < 0)
+		{
+			contadorNegativos++;
+			acumuladorNeg= acumuladorNeg + numero;
+		}
+		else if(numero >0)
+		{
+			contadorPositivos++;
+			acumuladorPos = acumuladorPos + numero;
+		}
+		else
+		{
+			contadorCeros++;
+		}
+		if( numero % 2 ==0)
+		{
+			contadorPares++;
+		}
+	
+		respuesta = prompt("Desea continuar?: ");
+	}while(respuesta =='s');
+
+	if( contadorPositivos !=0)
 	{
-		
-	
+		promedioPos = acumuladorPos / contadorPositivos;
 	}
+	if( contadorNegativos !=0)
+	{
+		promedioNeg = acumuladorNeg/ contadorNegativos;
+	}
+	diferencia = acumuladorPos - acumuladorNeg;
 
-
-
-
+	document.write("La suma de los negativos es "+ acumuladorNeg +"<br/>");
+	document.write("La suma de los positivos es "+ acumuladorPos +"<br/>");
+	document.write("La cantidad de los negativos es "+ contadorNegativos +"<br/>");
+	document.write("la cantidad de los positivos es "+ acumuladorNeg +"<br/>");
+	document.write("la cantidad de pares es "+ contadorPares +"<br/>");
+	document.write("La cantidad de ceros es  "+ contadorCeros +"<br/>");
+	document.write("El promedio de positvos es  "+ promedioPos +"<br/>");
+	document.write("El promedio de negativos es  "+ promedioNeg +"<br/>");
+	document.write("la diferencia entre negativos y positivos es"+ diferencia +"<br/>");
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,36 @@
 function Mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+	//var contador=0;
+	var mayor ;
+	var  menor ;
+	var numero;
+	var respuesta='s';
+	flag = 0;
+	do
+	{	
+		numero =parseInt(prompt("ingrese un numero"));
+	/*	contador++;
+		if(contador==1)
 
-	while(respuesta!='no')
-	{
-		
-	
-	}
+		{
+			mayor = numero
+			menor = numero
+		}*/
+		if (numero > mayor || flag ==0)
+		{
+			mayor = numero  ;
+		}
+		if(numero < menor || flag==0)
+		{
+			menor = numero
+			
+		}
+		flag = 1 
+		respuesta = prompt("Desea continuar?: ");
+	}while(respuesta =='s');
 
-
-
+	document.getElementById('maximo').value="El numero es " +mayor;
+	document.getElementById('minimo').value="El numero es" +menor;
 
 }//FIN DE LA FUNCIÓN
